9f0668366b73f079b670cc93dc2e30edb1f34f80
HaritsTama <nayotama1818@gmail.com>
2025-12-18 20:16:22 +0700

    Membuat DestinasiEntry.kt, HalamanHome.kt, dan PenyediaViewModel.kt

--- app/src/main/java/com/example/databaseremote/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/databaseremote/uicontroller/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.databaseremote.uicontroller.route
 
-object DestinasiEntry {
+import com.example.databaseremote.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
--- app/src/main/java/com/example/databaseremote/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/databaseremote/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.databaseremote.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.databaseremote.repositori.AplikasiDataSiswa
+import com.example.databaseremote.viewmodel.EntryViewModel
+
+fun CreationExtras.asplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(asplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { EntryViewModel(asplikasiDataSiswa().container.repositoryDataSiswa) }
+    }
+}
