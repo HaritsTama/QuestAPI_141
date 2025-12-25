8cbd279556f9f7dc81f0fb5a66640d99fa53ad50
HaritsTama <nayotama1818@gmail.com>
2025-12-25 16:34:17 +0700

    Menambahkan rute navigasi untuk Detail Siswa dan Edit Siswa

--- app/src/main/java/com/example/databaseremote/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/databaseremote/uicontroller/PetaNavigasi.kt
@@ -29,13 +29,26 @@ fun HostNavigasi(
         composable(DestinasiHome.route) {
             HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToItemUpdate = {
-//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                    navController.navigate("${DestinasiDetail.route}/${it}")
                 })
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
                 .route) })
         }
-
+        composable(DestinasiDetail.routeWithArgs,arguments = listOf(navArgument(DestinasiDetail
+            .itemIdArg) {
+            type = NavType.IntType })
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}
+                /$it")},
+                navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg
+            ){
+            type = NavType.IntType}){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
+        }
     }
 }
