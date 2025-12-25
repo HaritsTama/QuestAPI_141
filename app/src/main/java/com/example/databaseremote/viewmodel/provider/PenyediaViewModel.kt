07587ba46f7678ca7b75ffc7321daba999462004
HaritsTama <nayotama1818@gmail.com>
2025-12-25 17:02:51 +0700

    Melakukan update pada PenyediaViewModel.kt

--- app/src/main/java/com/example/databaseremote/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/databaseremote/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.databaseremote.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.databaseremote.repositori.AplikasiDataSiswa
+import com.example.databaseremote.viewmodel.DetailViewModel
+import com.example.databaseremote.viewmodel.EditViewModel
 import com.example.databaseremote.viewmodel.EntryViewModel
 import com.example.databaseremote.viewmodel.HomeViewModel
 
@@ -15,5 +18,17 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(asplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { EntryViewModel(asplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                asplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                asplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
